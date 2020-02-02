8568769a31c96fa35b72a7e59f885e51e731d419
Gabriel Florit <gabrielflorit@gmail.com>
2020-02-01 21:14:54 -0500

    Remove old code

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,31 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { navigate } from '@reach/router'
 import Nav from './Nav'
-import throwError from '../utils/throwError'
 
 const Shelf = props => {
   const { gist, setGist } = props
   const [cassettes, setCassettes] = useState([])
 
   useEffect(() => {
-    // window
-    //   .fetch(
-    //     'https://api.github.com/users/gabrielflorit/gists?since=2020-02-01T21:52:00Z',
-    //   )
-    //   .then(
-    //     response => response.json(),
-    //     error =>
-    //       throwError({
-    //         error,
-    //         message: `Could not fetch list of gists.`,
-    //       }),
-    //   )
-    //   .then(json => {
-    //     setCassettes(json)
-    //   })
-    //   .catch(error => {
-    //     console.log({ error })
-    //   })
     setCassettes([
       { id: 'e4caf0c2cb95499341d1731912c240f8' },
       { id: '9b0be7b96b75be367d28b51ecb0a7e55' },
