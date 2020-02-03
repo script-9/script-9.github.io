16ca446223852c1332cf8e52317245f59566b47b
Gabriel Florit <gabrielflorit@gmail.com>
2020-02-02 21:36:37 -0500

    Updates

---
+++ precache-manifest.276c1624df2bcad5df1ca7d1794880d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c151ea539e694e52e9febf1be097acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcfdb9a9876c54c3a84e",
+    "url": "/static/css/main.9899a67f.chunk.css"
+  },
+  {
+    "revision": "83a9e2cf3b5f4dbd51a5",
+    "url": "/static/js/2.79ae724e.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/static/js/2.79ae724e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcfdb9a9876c54c3a84e",
+    "url": "/static/js/main.757721de.chunk.js"
+  },
+  {
+    "revision": "fe494c930513ade041ad",
+    "url": "/static/js/runtime-main.180b83f4.js"
+  },
+  {
+    "revision": "e3552630d2013510a2427685ac569e87",
+    "url": "/static/media/VT323-latin.e3552630.woff2"
+  }
+]);
--- src/utils/getIdbValues.js
+++
@@ -1,6 +0,0 @@
-import * as idb from 'idb-keyval'
-
-const getIdbValues = () =>
-  idb.keys().then(keys => Promise.all(keys.map(key => idb.get(key))))
-
-export default getIdbValues
